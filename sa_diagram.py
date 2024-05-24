9a24b9fc5d4e5249ad577454ad49231956ea4ec6
vn56rn4 <vn56rn4@email.wal-mart.com>
2024-05-24 00:37:32 -0700

    Update system diagram

--- sa_diagram.py
+++ sa_diagram.py
@@ -7,6 +7,8 @@ from diagrams.onprem.database import PostgreSQL
 from diagrams.onprem.network import Zookeeper
 from diagrams.onprem.client import Client
 from diagrams.onprem.container import Docker
+from diagrams.c4 import Database
+
 graph_attr = {
     "splines": "splines"
 }
@@ -15,19 +17,30 @@ with Diagram("System Architecture", show=False, graph_attr=graph_attr):
     client = Client("Client Transaction")
     controller = Client("Control Center")
 
-    with Cluster("Containerized applications"):
+    with Cluster("Containerized applications", direction="TB"):
         kafka = Kafka("Kafka streaming")
         flink = Flink("Flink stream-processing") 
-        postgres = PostgreSQL("Data store")
+        # postgres = PostgreSQL("Data store")
         el_search = Elasticsearch("Elastissearch")
         kibana = Kibana("Data-Streaming Dashboard")
         zookeeper = Zookeeper("ZooKeeper")
-        docker = Docker("Docker")
-        
+        # docker = Docker("Docker")
     
+    with Cluster("Postgres Database"):
+        transactions = Database("transactions", description="Table")
+        sales_per_category = Database("sales_per_category", description="Table")
+        sales_per_day = Database("sales_per_day", description="Table")
+        sales_per_month = Database("sales_per_month", description="Table")
+    
+
     controller >> kafka
     client >> kafka >> zookeeper
     zookeeper>> kafka >> flink
 
-    flink >> postgres
+    flink >> [
+        transactions,
+        sales_per_category,
+        sales_per_day,
+        sales_per_month,
+    ]
     flink >> el_search  >> kibana
